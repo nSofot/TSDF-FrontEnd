46ca10b189e4534f00cfd0f643c9cf3974d72e4e
nSofot <nihalranathunge@gmail.com>
2025-08-24 13:55:06 +0530

    Initiate Project 👌

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tailwindcss(),
+  ]
+})
+
+// 1033564119689-va316rn8puf27vikpjmcl1v09qptfapl.apps.googleusercontent.com
+// 1033564119689-va316rn8puf27vikpjmcl1v09qptfapl.apps.googleusercontent.com
+// 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
